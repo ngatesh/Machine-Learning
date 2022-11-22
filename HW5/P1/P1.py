97e3da281befae3c1618e7583848144b1075f1c8
Nate Gatesh <ngatesh@asu.edu>
2022-11-21 18:43:28 -0700

    better accuracy metrics

--- HW5/P1/P1.py
+++ HW5/P1/P1.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import mean_absolute_error, make_scorer
+from sklearn.neighbors import KNeighborsClassifier
 
 # P1 - K Nearest Neighbor
 
@@ -13,26 +12,26 @@ dataset = np.loadtxt("seeds_dataset.txt", delimiter="\t", dtype=float)
 X = dataset[:, 0:7]
 y = dataset[:, 7].astype(int)
 
-k = range(1, 20)
-errors = np.zeros(19)
+k = range(1, 50)
+errors = np.zeros(49)
 
 for i in k:
     # Cross validation
     cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=8, random_state=7)
     # Regressor model
-    knn = KNeighborsRegressor(n_neighbors=i)
+    knn = KNeighborsClassifier(n_neighbors=i)
 
     # Score the model with cross validation
-    n_scores = cross_val_score(knn, X, y, scoring=make_scorer(mean_absolute_error), cv=cv, n_jobs=-1)
+    n_scores = cross_val_score(knn, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
 
     # Print results
     print(f"K-Nearest-Neighbor (k={i})")
-    print(f'\t> Mean Abs. Error (stdev): {np.average(n_scores):.3f} ({np.std(n_scores):.3f})')
+    print(f'\t> Mean Accuracy (stdev): {np.average(n_scores):.3f} ({np.std(n_scores):.3f})')
 
     errors[i-1] = np.average(n_scores)
 
 plt.scatter(k, errors)
 plt.title("KNN - Error vs. Number of Neighbors")
 plt.xlabel('Neighbors')
-plt.ylabel('Mean Absolute Error')
+plt.ylabel('Accuracy')
 plt.show()
